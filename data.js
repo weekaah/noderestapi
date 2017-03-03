c80fef894534256308708c095ddddad7e0eaab70
Viktor Lesic <weekaah@gmail.com>
2017-03-03 15:42:28 +0100

    add, edit and delete data

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 const data = [
   {
     "id": 1,
-    "first_name": "Ryan",
-    "last_name": "Chenkie",
+    "firstName": "Ryan",
+    "lastName": "Chenkie",
     "email": "ryan@scotch.io",
     "website": "http://ryanchenkie.com"
   },
   {
     "id": 2,
-    "first_name": "Chris",
-    "last_name": "Sevilleja",
+    "firstName": "Chris",
+    "lastName": "Sevilleja",
     "email": "chris@scotch.io",
     "website": "http://scotch.io"
   },
   {
     "id": 3,
-    "first_name": "Nick",
-    "last_name": "Cerminara",
+    "firstName": "Nick",
+    "lastName": "Cerminara",
     "email": "nick@scotch.io",
     "website": "http://scotch.io"
   },
   {
     "id": 4,
-    "first_name": "Ado",
-    "last_name": "Kukic",
+    "firstName": "Ado",
+    "lastName": "Kukic",
     "email": "ado@scotch.io",
     "website": "http://scotch.io"
   },
   {
     "id": 5,
-    "first_name": "Holly",
-    "last_name": "Lloyd",
+    "firstName": "Holly",
+    "lastName": "Lloyd",
     "email": "holly@scotch.io",
     "website": "http://scotch.io"
   }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 const express = require('express'),
       app = express(),
       hostname = 'localhost',
-      port = 3001;
+      port = 3001,
       bodyParser = require('body-parser'),
       cors = require('cors');
       
@@ -18,7 +18,7 @@ let contacts = require('./data');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
       
-// getting data from api
+// get data
 // ====================================    
 app.get('/api/contacts', (request, response) => {
   if (!contacts) {
@@ -30,13 +30,61 @@ app.get('/api/contacts', (request, response) => {
 app.get('/api/contacts/:id', (request, response) => {
   const requestId = request.params.id;
   
-  let contact = contacts.filter(contact => contact.id == requestId );
+  let contact = contacts.filter(contact => contact.id == requestId);
   
   if (!contacts) {
     response.status(404).json({message: 'No contact found'});
   }
   
   response.json(contact[0]);
+});
+
+// add data
+// ====================================    
+app.post('/api/contacts', (request, response) => {
+  const contact = {
+    id: contacts.length + 1,
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    website: request.body.website
+  };
+  
+  contacts.push(contact);
+  
+  response.json(contact);
+});
+
+// edit data
+// ====================================    
+app.put('/api/contacts/:id', (request, response) => {
+  const requestId = request.params.id;
+  
+  let contact = contacts.filter(contact => contact.id == requestId)[0];
+  
+  const index = contacts.indexOf(contact);
+  
+  const keys = Object.keys(request.body);
+  
+  keys.forEach(key => {
+    contact[key] = request.body[key];
+  });
+  
+  contacts[index] = contact;
+  
+  response.json(contacts[index]);
+});
+
+// delete data
+// ====================================    
+app.delete('/api/contacts/:id', (request, response) => {
+  const requestId = request.params.id;  
+  let contact = contacts.filter(contact => contact.id == requestId)[0];
+  const index = contacts.indexOf(contact);
+  
+  contacts.splice(index, 1);
+  
+  response.json({message: `User ${requestId} deleted`});
 });
 
 // start server
